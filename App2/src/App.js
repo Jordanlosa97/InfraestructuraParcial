57aec593aa79bb0fe0b8a5dbef58a8961387d77c
Bryanalmo <bryanalmo@unisabana.edu.co>
2020-05-20 10:53:35 -0500

    App2

---
+++ App2/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Route from './Containers/Routing.js';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <Route/>
+    </div>
+  );
+}
+
+export default App;
---
+++ App2/src/Intances/firebase.js
@@ -0,0 +1,23 @@
+import * as firebase from 'firebase/app';
+import 'firebase/firebase-app';
+import "firebase/auth";
+import "firebase/firestore";
+import "firebase/firebase-database";
+var firebaseConfig = {
+    apiKey: "AIzaSyBRL7Rz_clbVUliGgmhE-QAsF0Z9seCUL0",
+    authDomain: "proyectobienestar-e39bb.firebaseapp.com",
+    databaseURL: "https://proyectobienestar-e39bb.firebaseio.com",
+    projectId: "proyectobienestar-e39bb",
+    storageBucket: "proyectobienestar-e39bb.appspot.com",
+    messagingSenderId: "1073706496802",
+    appId: "1:1073706496802:web:22b64cd4e1011d391c2b7b",
+    measurementId: "G-Y1L7L7VB2V"
+  };
+  // Initialize Firebase
+
+  var Firebase = firebase.initializeApp(firebaseConfig);
+  
+  export var provider = new firebase.auth.OAuthProvider('microsoft.com');
+  export default Firebase
+  
+  
